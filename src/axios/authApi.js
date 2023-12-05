cd6e86cdc00164205ac0fd8f061e02dd902f25f7
hyewon-han <hw1635@hanmail.net>
2023-12-05 19:49:50 +0900

    interceptors 추가

--- src/axios/authApi.js
+++ src/axios/authApi.js
@@ -2,6 +2,29 @@ import axios from "axios";
 
 const authApi = axios.create({
   baseURL: "https://moneyfulpublicpolicy.co.kr",
+  timeout: 1000,
 });
+
+authApi.interceptors.request.use(
+  (config) => {
+    console.log("auth-server 요청 성공!");
+    return config;
+  },
+  (error) => {
+    console.log("auth-server 요청 실패!");
+    return Promise.reject(error);
+  }
+);
+
+authApi.interceptors.response.use(
+  (response) => {
+    console.log("auth-server 응답 받았다!");
+    return response;
+  },
+  (error) => {
+    console.log("auth-server 응답 못받았다!");
+    return Promise.reject(error);
+  }
+);
 
 export default authApi;
--- src/axios/jsonApi.js
+++ src/axios/jsonApi.js
@@ -4,4 +4,26 @@ const jsonApi = axios.create({
   baseURL: "https://serious-shade-weeder.glitch.me",
 });
 
+jsonApi.interceptors.request.use(
+  (config) => {
+    console.log("json-server 요청 성공!");
+    return config;
+  },
+  (error) => {
+    console.log("json-server 요청 실패!");
+    return Promise.reject(error);
+  }
+);
+
+jsonApi.interceptors.response.use(
+  (response) => {
+    console.log("json-server 응답 받았다!");
+    return response;
+  },
+  (error) => {
+    console.log("json-server 응답 못받았다!");
+    return Promise.reject(error);
+  }
+);
+
 export default jsonApi;
